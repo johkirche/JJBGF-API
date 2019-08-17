b315e95a32ba4a06eebc6730eb8282b10ed7bfb1
Manuel <hello@m-k.io>
2019-08-17 16:03:01 +0200

    feat: update configs

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,32 @@
-const jwt = require('jsonwebtoken')
-const APP_SECRET = 'GraphQL-is-aw3some'
+const jwt = require("jsonwebtoken");
+const APP_SECRET = "GraphQL-is-aw3some";
 
-function auth (context) {
-  const Authorization = context.request.get('Authorization')
+function auth(context) {
+  const Authorization = context.request.get("Authorization");
   if (Authorization) {
-    const token = Authorization.replace('Bearer ', '')
-    const {
-      userId
-    } = jwt.verify(token, APP_SECRET)
-    return userId
+    const token = Authorization.replace("Bearer ", "");
+    const { userId } = jwt.verify(token, APP_SECRET);
+    return userId;
   }
 
-  throw new Error('Not authenticated')
+  throw new Error("Not authenticated");
 }
 
-async function isAdmin (context) {
-  const userId = auth(context)
-  const result = await context.prisma.query.user({
-    where: {
-      id: userId
-    }
-  }, '{admin}')
-  return result.admin
+async function isAdmin(context) {
+  const userId = auth(context);
+  const result = await context.prisma.query.user(
+    {
+      where: {
+        id: userId
+      }
+    },
+    "{admin}"
+  );
+  return result.admin;
 }
 
 module.exports = {
   APP_SECRET,
   auth,
   isAdmin
-}
+};
