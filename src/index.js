a7fc963fa7ac34ff0cdfcfc64aa27fd54dd35c33
Manuel <hello@m-k.io>
2019-08-17 21:35:43 +0200

    feat: update cors

--- src/index.js
+++ src/index.js
@@ -30,7 +30,10 @@ const server = new GraphQLServer({
 });
 
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost, m-k.de"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:8080, https://jjbgf.de"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
